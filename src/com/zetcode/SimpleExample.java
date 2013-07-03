28d071fc05581aa09868e4b88382fb53bc8fdd32
Filip Turkiewicz <filip.turkiewicz+github@gmail.com>
2013-07-03 08:48:13 +0200

    cleaning

---
+++ src/com/zetcode/SimpleExample.java
@@ -0,0 +1,29 @@
+package com.zetcode;
+
+import javax.swing.*;
+
+/**
+ * SimpleExample
+ */
+public class SimpleExample extends JFrame {
+
+	public SimpleExample() {
+
+		setTitle("Simple example");
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+
+	public static void main(String[] args) {
+
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				SimpleExample ex = new SimpleExample();
+				ex.setVisible(true);
+			}
+		});
+	}
+}
