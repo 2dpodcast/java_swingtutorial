5e0486cbfccab9a5f2c00be7d31d9a973cce76b1
Filip Turkiewicz <filip.turkiewicz+github@gmail.com>
2013-08-01 15:34:29 +0200

    tetris

---
+++ src/com/zetcode/tetris/Tetris.java
@@ -0,0 +1,48 @@
+package com.zetcode.tetris;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Tetris
+ */
+public class Tetris extends JFrame {
+
+	private JLabel statusbar;
+
+	public Tetris() {
+		initUI();
+	}
+
+	private void initUI() {
+
+		statusbar = new JLabel(" 0");
+		add(statusbar, BorderLayout.SOUTH);
+
+		Board board = new Board(this);
+		add(board);
+		board.start();
+
+		setTitle("Tetris");
+		setSize(200, 400);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+
+	public JLabel getStatusBar() {
+		return statusbar;
+	}
+
+	public static void main(String[] args) {
+
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				Tetris game = new Tetris();
+				game.setVisible(true);
+			}
+		});
+	}
+}
